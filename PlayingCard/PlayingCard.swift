913289258dd5a043eb4360bf31843915708d53d3
Cesar Gutierrez Carrero <cesarguti47@gmail.com>
2018-02-05 18:33:04 +0100

    Currently the same that at the end of lecture 5.

---
+++ PlayingCard/PlayingCard.swift
@@ -0,0 +1,57 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by Cesar Gutierrez Carrero on 5/2/18.
+//  Copyright © 2018 Cesar Gutierrez Carrero. All rights reserved.
+//
+
+import Foundation
+
+// Nice protocol for printing things in the console
+struct PlayingCard: CustomStringConvertible
+{
+    var description: String { return "\(rank)\(suit)" }
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        var description: String
+        
+        case spades = "♠️"
+        case hearts = "♥️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+        
+        static var all = [Suit.spades, .hearts, .diamonds, .clubs]
+    }
+    
+    enum Rank: CustomStringConvertible {
+        var description: String
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+                case .ace: return 1
+                case .numeric(let pips): return pips
+                case .face(let kind) where kind == "J": return 11
+                case .face(let kind) where kind == "Q": return 12
+                case .face(let kind) where kind == "K": return 13
+                default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks = [Rank.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"), .face("Q"), .face("K")]
+            return allRanks
+        }
+    }
+}
---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Cesar Gutierrez Carrero on 5/2/18.
+//  Copyright © 2018 Cesar Gutierrez Carrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+}
+
